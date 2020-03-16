d1592e8ca31f436346035db41acd1a845dde82a8
bangss <973560782@qq.com>
2020-03-16 22:16:03 +0800

    Initial commit

---
+++ src/main/java/com/lingnan/ymzgzyz/YmzgzyzApplication.java
@@ -0,0 +1,13 @@
+package com.lingnan.ymzgzyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmzgzyzApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(YmzgzyzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lingnan/ymzgzyz/YmzgzyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingnan.ymzgzyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmzgzyzApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
