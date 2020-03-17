97cfd442f99bdd09ef242eb299a922a2c8b99967
bangss <973560782@qq.com>
2020-03-17 20:17:49 +0800

    2020.3.17

--- src/main/java/com/lingnan/ymzgzyz/YmzgzyzApplication.java
+++ src/main/java/com/lingnan/ymzgzyz/YmzgzyzApplication.java
@@ -1,8 +1,10 @@
 package com.lingnan.ymzgzyz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.lingnan.ymzgzyz.mapper")
 @SpringBootApplication
 public class YmzgzyzApplication {
 
