251c90ba6229ea2a063b40f23f7a9236f5b36a1f
bangss <973560782@qq.com>
2020-05-09 16:48:11 +0800

    2020.3.17  重新生成代码，每个controller实现基本CURD功能

--- src/test/java/com/lingnan/ymzgzyz/YmzgzyzApplicationTests.java
+++ src/test/java/com/lingnan/ymzgzyz/YmzgzyzApplicationTests.java
@@ -1,33 +1,33 @@
-package com.lingnan.ymzgzyz;
-
-
-import com.lingnan.ymzgzyz.model.entity.Admin;
-import com.lingnan.ymzgzyz.service.IAdminService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class YmzgzyzApplicationTests {
-
-    @Autowired
-    private IAdminService adminService;
-
-    @Test
-    public void contextLoads ( ) {
-    }
-
-    /**
-     * 测试数据库连接
-     */
-    @Test
-    public void testConnectionDataSource(){
-        Admin admin = adminService.getById(1);
-        System.out.println(admin);
-    }
-
-}
+//package com.lingnan.ymzgzyz;
+//
+//
+//import com.lingnan.ymzgzyz.model.entity.Admin;
+//import com.lingnan.ymzgzyz.service.IAdminService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//public class YmzgzyzApplicationTests {
+//
+//    @Autowired
+//    private IAdminService adminService;
+//
+//    @Test
+//    public void contextLoads ( ) {
+//    }
+//
+//    /**
+//     * 测试数据库连接
+//     */
+//    @Test
+//    public void testConnectionDataSource(){
+//        Admin admin = adminService.getById(1);
+//        System.out.println(admin);
+//    }
+//
+//}
